756714089013623df1ca00a6fed3154a1f9a89e6
Shogu23 <charles.marthouret@gmail.com>
2020-09-21 14:38:08 +0200

    trycatch add

--- src/Controller/MiscController.php
+++ src/Controller/MiscController.php
@@ -3,23 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MiscController extends AbstractController
 {
 
     private function httpPost($url, $data){
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
+        try {
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return $response;
+        } catch (Exception $e) {
+            echo "Error caught: " . $e->getMessage();
+      }
     } 
 
     /**
-     * @Route("/misc/sms", name="sms_misc")
+     * @Route("/misc/sms", name="sms_send")
      */
     public function sms()
     {
